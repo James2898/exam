1fedb2b8b8b7e416c4cf12f79ed255d2dea5cf61
James Martinez <amajamesmartinez@gmail.com>
2021-11-10 11:45:39 +0800

    Login and Register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -46,10 +46,7 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
+                <a href="{{ route('login') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150'">Login</a>
                 <x-button class="ml-4">
                     {{ __('Register') }}
                 </x-button>
